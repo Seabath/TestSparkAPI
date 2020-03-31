31f43dddec6954356db6ebcd5ea280dda40a953f
sfontanel <sfontanel@deezer.com>
2020-03-31 09:42:24 +0200

    DOC-add javadoc to SimpleService

--- src/main/java/service/SimpleService.java
+++ src/main/java/service/SimpleService.java
@@ -9,6 +9,11 @@ import org.apache.logging.log4j.Logger;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
+/**
+ * Generic service that interact with the DAO to manipulate the database.
+ *
+ * @param <T> Type of object you want to put / get from the database.
+ */
 public class SimpleService<T extends Serializable> {
 
     private static final Logger logger = LogManager.getLogger(SimpleService.class);
@@ -20,39 +25,81 @@ public class SimpleService<T extends Serializable> {
     }
 
 
+    /**
+     * Gets an entity with given id.
+     *
+     * @param id Id of the entity you want to retrieve.
+     * @return Entity matching given id. Null if error happened.
+     */
     public T get(Long id) {
         return this.executeQuery(session -> tSimpleDAO.find(session, id));
     }
 
+    /**
+     * Gets a list containing all entities from a table.
+     *
+     * @return All entities from a table. Null if error happened.
+     */
     public List<T> getAll() {
         return this.executeQuery(tSimpleDAO::findAll);
     }
 
+    /**
+     * Creates a new entry into the database with given entity.
+     *
+     * @param entity Entity to put into the database.
+     * @return Updated entity with its id filled. Null if error happened.
+     */
     public T create(T entity) {
         return this.executeQueryWithTransaction(session ->
             tSimpleDAO.save(session, entity));
     }
 
+    /**
+     * Creates or updates an entry into the database with given entity.
+     *
+     * @param entity Entity to put or update into the database.
+     * @return Updated entity with its id filled. Null if error happened.
+     */
     public T createOrUpdate(T entity) {
         return this.executeQueryWithTransaction(session ->
             tSimpleDAO.saveOrUpdate(session, entity));
     }
 
+    /**
+     * Deletes an entry from the database.
+     *
+     * @param entity Entity to put or update into the database.
+     * @return Deleted entity. Null if error happened.
+     */
     public T delete(T entity) {
         return this.executeQueryWithTransaction(session ->
             tSimpleDAO.delete(session, entity));
     }
 
+    /**
+     * Deletes an entry from the database with given id;
+     *
+     * @param id Id of the entity to delete from the database.
+     * @return Deleted entity. Null if error happened.
+     */
     public T delete(Long id) {
         return this.executeQueryWithTransaction(session ->
             tSimpleDAO.delete(session, tSimpleDAO.find(session, id)));
     }
 
 
-    protected <V> V executeQuery(Function<Session, V> callable) {
+    /**
+     * Opens a session from the dao and executes series of queries from the session.
+     *
+     * @param function Function containing queries to execute.
+     * @param <V>      Type of return of given function.
+     * @return Returns what given function supposed to return.
+     */
+    protected <V> V executeQuery(Function<Session, V> function) {
         V retValue;
         try (Session session = tSimpleDAO.openSession()) {
-            retValue = callable.apply(session);
+            retValue = function.apply(session);
         } catch (Exception e) {
             logger.error("Error while getting into the database", e);
             return null;
@@ -61,12 +108,21 @@ public class SimpleService<T extends Serializable> {
         return retValue;
     }
 
-    protected <V> V executeQueryWithTransaction(Function<Session, V> callable) {
+
+    /**
+     * Opens a session with transaction from the dao and executes series of queries from the
+     * session.
+     *
+     * @param function Function containing queries to execute.
+     * @param <V>      Type of return of given function.
+     * @return Returns what given function supposed to return.
+     */
+    protected <V> V executeQueryWithTransaction(Function<Session, V> function) {
         V retValue;
         Transaction transaction = null;
         try (Session session = tSimpleDAO.openSession()) {
             transaction = session.beginTransaction();
-            retValue = callable.apply(session);
+            retValue = function.apply(session);
             transaction.commit();
         } catch (Exception e) {
             if (transaction != null) {
