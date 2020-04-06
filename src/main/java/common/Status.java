a8df3693cbc1284d42d228711a8f806ea2fc8d76
sfontanel <sfontanel@deezer.com>
2020-04-06 09:54:37 +0200

    FEATURE: add changeSuccess method to status

--- src/main/java/common/Status.java
+++ src/main/java/common/Status.java
@@ -6,11 +6,36 @@ import lombok.Getter;
  * Status of tests and test suite.
  */
 public enum Status {
-    SUCCESS(0),
-    FLAKY(1),
-    FAIL(2),
-    IN_PROGRESS(3),
-    INTERRUPTED(4);
+    SUCCESS(0) {
+        @Override
+        public Status changeSuccess() {
+            return SUCCESS;
+        }
+    },
+    FLAKY(1) {
+        @Override
+        public Status changeSuccess() {
+            return FLAKY;
+        }
+    },
+    FAIL(2) {
+        @Override
+        public Status changeSuccess() {
+            return FLAKY;
+        }
+    },
+    IN_PROGRESS(3) {
+        @Override
+        public Status changeSuccess() {
+            return SUCCESS;
+        }
+    },
+    INTERRUPTED(4) {
+        @Override
+        public Status changeSuccess() {
+            return INTERRUPTED;
+        }
+    };
 
     /**
      * Field used to compare status one with another.
@@ -21,4 +46,11 @@ public enum Status {
     Status(int value) {
         this.value = value;
     }
+
+    /**
+     * Give new status for success state.
+     *
+     * @return New status
+     */
+    public abstract Status changeSuccess();
 }
